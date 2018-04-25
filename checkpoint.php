c71f027bb524ec3e58f928b92cf83e9eee721f2f
black <ironhighh@gmail.com>
2018-04-25 15:39:08 +0700

    log when checkpoint and can change pass

--- checkpoint.php
+++ checkpoint.php
@@ -2,29 +2,91 @@
   <body>
   <br>
 <?php
+date_default_timezone_set('Asia/Bangkok');
 // print_r($_POST);
-$authenPass = $_POST['authen_password'];
-if (((isset($_POST['authen_submit'])) && ($authenPass == '7852899')) || 
-((isset($_POST['checkPoint_submit']))) ) {
-  echo '<div id="authen_panel">
-  <form method="POST" name="checkPoint_form">
+
+if (isset($_POST['authen_submit'])) {
+  require_once 'conf/conn.php';
+  $authenUser = $_POST['authen_user'];
+  $authenPass = $_POST['authen_password'];
+  $queryUser = " SELECT nUserID, outletcode, expid, expdate, strUserName, position, typeuser, isManager, levelCode, emppassword
+  FROM  imp_emp2
+  WHERE (nUserID = ?) AND (emppassword = ?) AND (levelCode = 9) ";
+// echo $queryPoint;
+// $stmt = $conn->query( $queryPoint );
+$stmt = $conn->prepare( $queryUser, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+$stmt->bindParam(1, $authenUser,PDO::PARAM_STR);
+$stmt->bindParam(2, $authenPass,PDO::PARAM_STR);
+$stmt->execute();
+$userRowCount = $stmt->rowCount();
+// var_dump($pointRowCount);
+if ($userRowCount != 0) {
+
+  $result = $stmt->fetchall(PDO::FETCH_BOTH);
+  // var_dump($result);
+  echo '<div style="float:right;background-color:silver;">
+  <h3>เปลี่ยนรหัสผ่าน</h3>
+  <form method="POST" name="changePassword_form">
+  <input type="text" id="changepassword_pass" name="changepassword_pass" placeholder="ระบุรหัสที่ต้องการเปลี่ยน">
+  <input type="hidden"  name="changepassword_user" value="'.$authenUser.'">
+  <button type="submit" name="changePassword_submit">ChangePassword</button>
+  </div>';
+
+  
+  echo '<div id="authen_panel">';
+  echo '<h2>ยินดีต้อนรับ '.$result[0]['strUserName'].'</h2>';
+  echo '<form method="POST" name="checkPoint_form">
   <h3>CustomerID</h3>
     <label id="customerid-error" class="error" for="customerid"></label><br>
-		<input type="number" id="customerid" name="customerid" placeholder="ระบุรหัสบัตร" required>
+		<input type="number" id="customerid" name="customerid" placeholder="ระบุรหัสบัตร">
 			<br>
 			<br>
 			<button type="submit" name="checkPoint_submit">checkpoint</button>
 		</form>
 		</div>
 		<br>';
+}else{
+  echo 'ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง';
+  echo '<br>';
+  echo '<input action="action" onclick="window.history.go(-1); return false;" type="button" value="Back" />';
+}
 }
 
-// print_r($_POST);
+// change password form
+if ((isset($_POST['changePassword_submit'])) && 
+($_POST['changepassword_pass'] != '')) {
+  // echo $_POST['changepassword_user'];
+  require_once 'conf/conn.php';
+$SQLChangePass = " UPDATE imp_emp2 SET emppassword = ? WHERE nUserID = ? ";
+$stmtChangePass = $conn->prepare( $SQLChangePass, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+$stmtChangePass->bindParam(1, $_POST['changepassword_pass'],PDO::PARAM_STR);
+$stmtChangePass->bindParam(2, $_POST['changepassword_user'],PDO::PARAM_STR);
+$status = $stmtChangePass->execute();
+if($status) {
+  echo 'เปลี่ยนรหัสผ่านสำเร็จ';
+  echo '<br>';
+  echo '<input action="action" onclick="window.history.go(-1); return false;" type="button" value="Back" />';
+}else {
+  echo 'เปลี่ยนรหัสผ่านไม่สำเร็จ';
+  echo '<br>';
+  echo '<input action="action" onclick="window.history.go(-1); return false;" type="button" value="Back" />';
+}
+}
 
+// checkpoint form
 $customerID = $_POST['customerid'];
-
 if ((isset($_POST['checkPoint_submit'])) && ($customerID != '') ) {
-	require_once 'conf/conn.php';
+  require_once 'conf/conn.php';
+    // Save log login
+    $saveLog = " INSERT INTO log_view_point (username, systemdate, customerid)
+    VALUES (?, ?, ?) ";
+    $stmtSaveLog = $conn->prepare( $saveLog, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+    $stmtSaveLog->bindParam(1, $_POST['changepassword_user'],PDO::PARAM_STR);
+    $systemdate = date("Y-m-d H:i:s");
+    $stmtSaveLog->bindParam(2, $systemdate,PDO::PARAM_STR);
+    $stmtSaveLog->bindParam(3, $customerID,PDO::PARAM_STR);
+    $stmtSaveLog->execute();
+
 $queryPoint = " SELECT     tcustomerlog_cal.customerid, 
 SUM(tcustomerlog_cal.addpoint - tcustomerlog_cal.delpoint) AS point, 
 T_Customer.CustomerName, T_Customer.CustomerLast, 
@@ -82,7 +144,7 @@ $stmt = $conn->prepare( $queryLog );
 $stmt->bindParam(1, $customerID,PDO::PARAM_STR);
 $stmt->execute();
 $logRowCount = $stmt->rowCount();
-
+echo '<input action="action" onclick="window.history.go(-1); return false;" type="button" value="Back" />';
 if($logRowCount != 0) {
 
 echo '<h3>ข้อมูลการใช้บัตรสมาชิก</h3>
@@ -111,16 +173,14 @@ echo '</table>';
 }
 else{
   echo 'ไม่พบข้อมูลการใช้บัตรสมาชิก';
+  echo '<input action="action" onclick="window.history.go(-1); return false;" type="button" value="Back" />';
 }
 
 }else{
   echo 'ไม่พบข้อมูล';
+  echo '<input action="action" onclick="window.history.go(-1); return false;" type="button" value="Back" />';
 }
-}elseif($authenPass != '7852899') {
-  echo 'รหัสผ่านไม่ถูกต้อง';
 }
-
-
 ?>
 
 </table>
--- index.php
+++ index.php
@@ -3,8 +3,13 @@
 		<h1 class="text-center">Sukishi Checkpoint Online</h1>
 		<br>
 		<form action="checkpoint.php" name="authen_form" method="post">
-		<input type="password" name="authen_password">
-		<button type="submit" name="authen_submit">Authen</button>
+		<h2>User</h2>
+		<input type="text" name="authen_user" placeholder="user..." required>
+		<h2>Password</h2>
+		<input type="password" name="authen_password" placeholder="pass..." required>
+		<br>
+		<br>
+		<button type="submit" name="authen_submit">login</button>
 		</form>
 		<br>
 	</body>
